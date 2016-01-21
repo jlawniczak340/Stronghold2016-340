d2ff88428d5adeb78c8611c5857e1512ba41167e
John Lawniczak <jlawniczak340@gmail.com>
2016-01-21 18:56:34 -0500

    Update OI.java

--- test/src/org/usfirst/frc/team340/robot/OI.java
+++ test/src/org/usfirst/frc/team340/robot/OI.java
@@ -2,7 +2,7 @@ package org.usfirst.frc.team340.robot;
 
 import edu.wpi.first.wpilibj.buttons.Button;
 import org.usfirst.frc.team340.robot.commands.ExampleCommand;
-
+//this network stinks
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
